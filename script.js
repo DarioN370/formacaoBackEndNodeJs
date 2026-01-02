43a1ad1052c85413224119cd952ebe603603f89b
DarioN370 <netobis2015@gmail.com>
2026-01-02 01:46:03 -0300

    updt folder

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ formas de declarar uma variavel
 1- Variavel Global (só coloca o nome e atribuí o valor)
 2- var (velha)
 3- let (nova)
+LET- Muda sem erro, porem só pode usar dentro do scopo da função, ou do if etc
+VAR- Mutavel em qualquer parte do codigo sem verificacao
 4- const (não vai mudar)
 */
 
