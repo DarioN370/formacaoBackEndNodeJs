ce8d97dcb7472299ef9107fefa6f25252b426e12
DarioN370 <netobis2015@gmail.com>
2026-01-02 01:43:43 -0300

    updt folder

---
+++ AtividadesModulo4/boasVindas.js
@@ -0,0 +1,3 @@
+let nome = 'Dario Miranda Cordeiro Neto'
+
+console.log('Olá', nome, '! Seja bem vindo ao curso de JavaScript!')
---
+++ AtividadesModulo4/calculoDeIdade.js
@@ -0,0 +1,6 @@
+let anoAtual = 2026;
+const anoNascimento = 2003;
+const idade = (anoAtual - anoNascimento);
+const frase = 'Você tem:';
+
+console.log(frase, idade, "Anos de idade");
