1ae3454cac0ac3ef6d02f0808ac5b68209810f80
DarioN370 <netobis2015@gmail.com>
2026-01-04 20:30:38 -0300

    terminando aulas antes dos exercicios e da aula de prefix e postfix

---
+++ modulo5Operadores/combinandoComp.js
@@ -0,0 +1,50 @@
+const idade = 22;
+const maiorDeIdade = idade >= 18;
+const possuiCNH = true;
+
+const podeDirigir = maiorDeIdade && possuiCNH; //&& AND 
+const podeViajarSolo = maiorDeIdade || possuiCNH; // || OR
+const precisaDeAcompanhante = !maiorDeIdade; // NOT, aqui ele inverte o valor, ou seja, qnd 'maiorDeIdade' for true, o 'precisaDeAcompanhante' vai ser false, e vice e versa
+
+console.log('Pode dirigir ?', podeDirigir);
+console.log('Pode viajar sozinha ?', podeViajarSolo);
+console.log('Precisa de acompanhante ?', precisaDeAcompanhante);
+
+// EXEMPLO AND - Entrar na FESTA
+
+if(maiorDeIdade && possuiCNH) {
+    console.log('Pode entrar na festa');
+} else {
+    console.log('Nao pode entrar, CAI FORA!!!');
+};
+
+// EXEMPLO OR - Pode fazer a compra
+let temCartaoCredito = false;
+let temDinheiro = true;
+
+if(temCartaoCredito || temDinheiro) {
+    console.log('Pode fazer a compra');
+} else {
+    console.log('Ta duro dorme!!!');
+};
+
+// EXEMPLO NOT - Esta chovendo
+let estaChovendo = true;
+
+if (!estaChovendo) {
+    console.log('Pode sair sem guarda-chuva');
+} else {
+    console.log('Leva guarda-chuva');
+};
+
+// COMBINANDO TODOS OS OPERADORES
+
+let idadeFan = 20;
+let possuiIngresso = true;
+let entradaPermitida = (idadeFan >= 18 && possuiIngresso);
+
+if (entradaPermitida) {
+    console.log('Entrada Permitida');
+} else {
+    console.log('Entrada Negada');
+};
---
+++ modulo5Operadores/operadoresComp.js
@@ -0,0 +1,30 @@
+const idadeVini = 26;
+const minhaIdade = 22;
+
+// Igualdade
+// const comparacao = idadeVini == minhaIdade;
+
+// Maior que
+// const comparacao = idadeVini > minhaIdade;
+
+// Menor que
+// const comparacao = idadeVini < minhaIdade;
+
+// Maior ou igual a
+// const comparacao = idadeVini >= minhaIdade;
+
+// Menor ou igual a 
+// const comparacao = idadeVini <= minhaIdade;
+
+// Diferentes
+// const comparacao = idadeVini != minhaIdade;
+
+console.log('São Diferentes?: ', comparacao)
+
+
+
+// COMPARACAO ESTRITA, ou seja, ele não converte tipos
+minhaIdadeString = "22"
+
+const comparacaoEstrita = minhaIdadeString ===idadeVini
+console.log('São estritamente iguais? ', comparacaoEstrita)
