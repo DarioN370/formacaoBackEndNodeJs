3b6097ae6bdb0a222bd422cf2f15e6e0b23b531d
DarioN370 <netobis2015@gmail.com>
2026-01-04 02:54:23 -0300

    updt: separando curso por pastas

---
+++ modulo4Variaveis/AtividadesModulo4/mensagemDeLocal.js
@@ -0,0 +1,5 @@
+let cidade = 'São João da Boa Vista';
+let estado = 'São Paulo';
+let pais = 'Brasil';
+
+console.log("Você está em:", cidade, ',', estado,',', pais);
---
+++ modulo4Variaveis/AtividadesModulo4/simuladorBancario.js
@@ -0,0 +1,6 @@
+let saldo = 0;
+let deposito = 200;
+let saque = 50;
+const novoSaldo = saldo+deposito-saque;
+
+console.log('\nSeu saldo era:',saldo, '\nApós um depósito de:', deposito, ' \nE um saque de:', saque, '\nseu saldo agora é:', novoSaldo)
