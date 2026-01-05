30f02643b93c418ee76d0a70663662600684160b
DarioN370 <netobis2015@gmail.com>
2026-01-05 08:12:24 -0300

    add Unarios operators

---
+++ modulo5Operadores/operadoresUnarios.js
@@ -0,0 +1,9 @@
+// PREFIXADO E POSFIXADO
+
+//PRE
+let contador = 5;
+console.log(++contador); // imprime 6, pois a variável é incrementada antes da impressão
+
+//POS
+let contador2 = 5;
+console.log(contador++); // imprime 5 e só depois incrementa; ao final, contador passa a valer 6
